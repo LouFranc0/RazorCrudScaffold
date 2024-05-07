930850567ca9774f4008b5858278d8d2370b181d
Francesco Caputo <caputo.francesco001@gmail.com>
2024-05-07 14:57:46 +0200

    Aggiungere i file di progetto.

---
+++ RazorCrudScaffold/Model/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorCrudScaffold.Model
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [DisplayName("Employee Name")]
+
+        public string Name { get; set; }
+
+        [Required]
+        public string Surname { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+    }
+
+
+}
